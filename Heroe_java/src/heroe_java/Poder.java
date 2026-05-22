51fb6ab00f777b9e48b414a4900576d410f6a7c4
20223762@clases.edu.sv <20223762@clases.edu.sv>
2026-05-22 17:26:29 -0600

    feat: Forja del artefacto Poder y su desatación mágica

---
+++ Heroe_java/src/heroe_java/Poder.java
@@ -0,0 +1,13 @@
+package heroe_java;
+
+public class Poder {
+	    // Atributos del artefacto
+	    String nombre; // El título del hechizo [cite: 45]
+	    int danio;     // El impacto destructivo [cite: 46]
+
+	    // Encantamiento para activar el poder [cite: 47]
+	    public String activar() {
+	        return "¡El cielo se quiebra al liberar la Llama del Dragón causando 50 de daño!"; 
+	    }
+	}
+
